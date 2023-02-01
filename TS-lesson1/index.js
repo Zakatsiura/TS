6644d1345ae4667155e542a0b4b999fe6dfdc8e0
Zakatsiura <zakatsiura@gmail.com>
2023-02-01 22:11:59 +0200

    init

--- TS-lesson1/index.js
+++ TS-lesson1/index.js
@@ -1,11 +1,46 @@
-console.log('Hello');
-var message = 'Hello';
-function var2() {
-    for (var i = 0; i < 5; i++) {
-        setTimeout(function () {
-            console.log(i);
-            1000;
-        });
-    }
+var icecream = {
+    smallSize: 10,
+    bigSize: 25,
+    chokolade: 5,
+    caramel: 6,
+    berry: 10,
+    marsh: 5
+};
+var totalPrice = 0;
+var option1 = prompt('Which size would you like: enter S for small or B for big?');
+switch (option1) {
+    case 'S':
+        totalPrice = icecream.smallSize;
+        break;
+    case 'B':
+        totalPrice = icecream.bigSize;
+        break;
+    default:
+        alert('Incorrect input');
 }
-var2();
+var option2 = prompt('What would you like: enter C for chocolate, R for caramel or Y for berry?');
+switch (option2) {
+    case 'C':
+        totalPrice += icecream.chokolade;
+        break;
+    case 'R':
+        totalPrice += icecream.caramel;
+        break;
+    case 'Y':
+        totalPrice += icecream.berry;
+        break;
+    default:
+        alert('Incorrect input');
+}
+var option3 = prompt('Would you like marshmallow? input Y or N');
+switch (option3) {
+    case 'Y':
+        totalPrice += icecream.berry;
+        break;
+    case 'N':
+        totalPrice += 0;
+        break;
+    default:
+        alert('Incorrect input');
+}
+alert("Total price is - ".concat(totalPrice));
--- TS-lesson1/index.ts
+++ TS-lesson1/index.ts
@@ -1,13 +1,54 @@
-console.log('Hello');
+const icecream = {
+  smallSize: 10,
+  bigSize: 25,
+  chokolade: 5,
+  caramel: 6,
+  berry: 10,
+  marsh: 5,
+};
+let totalPrice = 0;
 
-let message: string = 'Hello';
-
-function var2() {
-  for (var i: number = 0; i < 5; i++) {
-    setTimeout(function () {
-      console.log(i);
-      1000;
-    });
-  }
+let option1 = prompt(
+  'Which size would you like: enter S for small or B for big?'
+);
+switch (option1) {
+  case 'S':
+    totalPrice = icecream.smallSize;
+    break;
+  case 'B':
+    totalPrice = icecream.bigSize;
+    break;
+  default:
+    alert('Incorrect input');
 }
-var2();
+
+let option2 = prompt(
+  'What would you like: enter C for chocolate, R for caramel or Y for berry?'
+);
+switch (option2) {
+  case 'C':
+    totalPrice += icecream.chokolade;
+    break;
+  case 'R':
+    totalPrice += icecream.caramel;
+    break;
+  case 'Y':
+    totalPrice += icecream.berry;
+    break;
+  default:
+    alert('Incorrect input');
+}
+
+let option3 = prompt('Would you like marshmallow? input Y or N');
+switch (option3) {
+  case 'Y':
+    totalPrice += icecream.berry;
+    break;
+  case 'N':
+    totalPrice += 0;
+    break;
+  default:
+    alert('Incorrect input');
+}
+
+alert(`Total price is - ${totalPrice}`);
