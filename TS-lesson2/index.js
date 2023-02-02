153856037ca761d96db9dd3890d0d5eaeb22103b
Zakatsiura <zakatsiura@gmail.com>
2023-02-02 17:40:28 +0200

    1 task is done

--- TS-lesson2/index.js
+++ TS-lesson2/index.js
@@ -16,32 +16,80 @@ var __extends = (this && this.__extends) || (function () {
 var Car = /** @class */ (function () {
     function Car(name) {
         this.name = name;
+        this.bonus = 'additional equipment is available';
     }
-    Car.prototype.price = function () {
-        console.log(this.price + '$');
-    };
-    Car.prototype.engPower = function () {
-        console.log(this.engPower + 'h.p.');
-    };
     return Car;
 }());
-var Sport = /** @class */ (function (_super) {
-    __extends(Sport, _super);
-    function Sport() {
-        return _super.call(this, 'Sport Car') || this;
+var Audi = /** @class */ (function (_super) {
+    __extends(Audi, _super);
+    function Audi() {
+        var _this = _super.call(this, 'Audi') || this;
+        _this.discount = -500;
+        _this.bonus = 'none';
+        return _this;
     }
-    Sport.prototype.carClass = function () {
-        console.log('Sport car');
+    Audi.prototype.model = function () {
+        console.log(this.name);
     };
-    Sport.prototype.price = function () {
-        console.log(1000);
-    };
-    Sport.prototype.engPower = function () {
-        console.log(500);
-    };
-    return Sport;
+    return Audi;
 }(Car));
-var ferrari = new Sport();
-ferrari.carClass();
-ferrari.price();
-ferrari.engPower();
+var BMW = /** @class */ (function (_super) {
+    __extends(BMW, _super);
+    function BMW() {
+        var _this = _super.call(this, 'BMW') || this;
+        _this.discount = -800;
+        return _this;
+    }
+    BMW.prototype.model = function () {
+        console.log(this.name);
+    };
+    return BMW;
+}(Car));
+var Mercedes = /** @class */ (function (_super) {
+    __extends(Mercedes, _super);
+    function Mercedes() {
+        var _this = _super.call(this, 'Mercedes') || this;
+        _this.discount = -700;
+        return _this;
+    }
+    Mercedes.prototype.model = function () {
+        console.log(this.name);
+    };
+    return Mercedes;
+}(Car));
+var audiA6 = new Audi();
+audiA6.model();
+audiA6.price = 10000;
+audiA6.type = 'Audi A6';
+audiA6.power = 350;
+console.log(audiA6);
+var audiA8 = new Audi();
+audiA8.model();
+audiA8.price = 15000;
+audiA8.type = 'Audi A8';
+audiA6.power = 380;
+console.log(audiA8);
+var bmw750 = new BMW();
+bmw750.model();
+bmw750.price = 18000;
+bmw750.type = 'BMW 750';
+bmw750.power = 410;
+console.log(bmw750);
+var bmw530 = new BMW();
+bmw530.model();
+bmw530.price = 14500;
+bmw530.type = 'BMW 530';
+bmw530.power = 260;
+console.log(bmw530);
+var mercedes550 = new Mercedes();
+mercedes550.model();
+mercedes550.price = 25500;
+mercedes550.type = 'S 550';
+mercedes550.power = 480;
+console.log(mercedes550);
+var mercedes600 = new Mercedes();
+mercedes600.model();
+mercedes600.price = 29500;
+mercedes600.type = 'S 600';
+mercedes600.power = 520;
+console.log(mercedes600);
--- TS-lesson2/index.ts
+++ TS-lesson2/index.ts
@@ -1,30 +1,93 @@
 abstract class Car {
   constructor(public name: string) {}
-  abstract carClass();
-  public price(): void {
-    console.log(this.price + '$');
-  }
-  public engPower(): void {
-    console.log(this.engPower + 'h.p.');
-  }
+  abstract model();
+  protected bonus: string = 'additional equipment is available';
 }
 
-class Sport extends Car {
+class Audi extends Car {
   constructor() {
-    super('Sport Car');
+    super('Audi');
   }
-  carClass() {
-    console.log('Sport car');
+  model() {
+    console.log(this.name);
   }
-  price() {
-    console.log(1000);
-  }
-  engPower() {
-    console.log(500);
-  }
+  public price: number;
+  public type: string;
+  public power: number;
+  private discount: number = -500;
+  bonus: string = 'none';
 }
 
-let ferrari: Sport = new Sport();
-ferrari.carClass();
-ferrari.price();
-ferrari.engPower();
+class BMW extends Car {
+  constructor() {
+    super('BMW');
+  }
+  model() {
+    console.log(this.name);
+  }
+  public price: number;
+  public type: string;
+  public power: number;
+  private discount: number = -800;
+}
+
+class Mercedes extends Car {
+  constructor() {
+    super('Mercedes');
+  }
+  model() {
+    console.log(this.name);
+  }
+  public price: number;
+  public type: string;
+  public power: number;
+  private discount: number = -700;
+}
+
+let audiA6: Audi = new Audi();
+audiA6.model();
+audiA6.price = 10000;
+audiA6.type = 'Audi A6';
+audiA6.power = 350;
+
+console.log(audiA6);
+
+let audiA8: Audi = new Audi();
+audiA8.model();
+audiA8.price = 15000;
+audiA8.type = 'Audi A8';
+audiA6.power = 380;
+
+console.log(audiA8);
+
+let bmw750: BMW = new BMW();
+bmw750.model();
+bmw750.price = 18000;
+bmw750.type = 'BMW 750';
+bmw750.power = 410;
+
+console.log(bmw750);
+
+let bmw530: BMW = new BMW();
+bmw530.model();
+bmw530.price = 14500;
+bmw530.type = 'BMW 530';
+bmw530.power = 260;
+
+console.log(bmw530);
+
+let mercedes550: Mercedes = new Mercedes();
+mercedes550.model();
+mercedes550.price = 25500;
+mercedes550.type = 'S 550';
+mercedes550.power = 480;
+
+console.log(mercedes550);
+
+let mercedes600: Mercedes = new Mercedes();
+mercedes600.model();
+mercedes600.price = 29500;
+mercedes600.type = 'S 600';
+mercedes600.power = 520;
+
+console.log(mercedes600);
