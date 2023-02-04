fe24accff892f4919b89a886bc6052657737bd80
Zakatsiura <zakatsiura@gmail.com>
2023-02-04 17:51:25 +0200

    init

--- TS-lesson3/index.js
+++ TS-lesson3/index.js
@@ -1,76 +1,52 @@
-var MyPowerDictionary = /** @class */ (function () {
-    function MyPowerDictionary() {
-        this._data = [];
+'use strict';
+class MyPowerDictionary {
+  constructor() {
+    this._data = [];
+  }
+  set key(value) {
+    if (value == null || value == undefined) {
+      throw new Error('Значение value не может быть пустым.');
     }
-    Object.defineProperty(MyPowerDictionary.prototype, "_key", {
-        set: function (value) {
-            if (value == null || value == undefined) {
-                throw new Error('Значение value не может быть пустым.');
-            }
-            this._key = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(MyPowerDictionary.prototype, "key", {
-        get: function () {
-            return this._key;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(MyPowerDictionary.prototype, "_value", {
-        set: function (value) {
-            if (value == null || value == undefined) {
-                throw new Error('Значение value не может быть пустым.');
-            }
-            this._value = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(MyPowerDictionary.prototype, "value", {
-        get: function () {
-            return this._value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(MyPowerDictionary.prototype, "_description", {
-        set: function (value) {
-            if (value == null || value == undefined) {
-                throw new Error('Значение value не может быть пустым.');
-            }
-            this._description = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(MyPowerDictionary.prototype, "description", {
-        get: function () {
-            return this._description;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    MyPowerDictionary.prototype.add = function (key, value, description) {
-        var entry = new MyPowerDictionary();
-        entry._key = key;
-        entry._value = value;
-        entry._description = description;
-        this._data.push(entry);
-    };
-    MyPowerDictionary.prototype.getValue = function (key) {
-        for (var i = 0; i < this._data.length; i++) {
-            if (this._data[i]._key == key) {
-                return this._data[i]._value;
-            }
-        }
-        return null;
-    };
-    return MyPowerDictionary;
-}());
-var dictionary = new MyPowerDictionary();
+    this._key = value;
+  }
+  get key() {
+    return this._key;
+  }
+  set value(value) {
+    if (value == null || value == undefined) {
+      throw new Error('Значение value не может быть пустым.');
+    }
+    this._value = value;
+  }
+  get value() {
+    return this._value;
+  }
+  set description(value) {
+    if (value == null || value == undefined) {
+      throw new Error('Значение value не может быть пустым.');
+    }
+    this._description = value;
+  }
+  get description() {
+    return this._description;
+  }
+  add(key, value, description) {
+    let entry = new MyPowerDictionary();
+    entry._key = key;
+    entry._value = value;
+    entry._description = description;
+    this._data.push(entry);
+  }
+  getValue(key) {
+    for (let i = 0; i < this._data.length; i++) {
+      if (this._data[i]._key == key) {
+        return this._data[i]._value;
+      }
+    }
+    return null;
+  }
+}
+let dictionary = new MyPowerDictionary();
 dictionary.add('hello', 'привет', 'приветствие');
 dictionary.add('book', 'книга', 'бумажный носитель информации');
 dictionary.add('apple', 'яблоко', 'фрукт');
